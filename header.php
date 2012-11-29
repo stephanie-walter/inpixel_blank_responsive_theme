4e5cfd6b19295c5adabb1b348102f0c038ad58be
Stephanie Walter <stephanie.walter@inpixelitrust.fr>
2012-11-29 23:50:25 +0100

    menu revamp

--- header.php
+++ header.php
@@ -71,7 +71,7 @@
 				<div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'themename' ); ?>"><?php _e( 'Skip to primary content', 'themename' ); ?></a></div>
 				<div class="skip-link"><a class="assistive-text" href="#secondary" title="<?php esc_attr_e( 'Skip to secondary content', 'themename' ); ?>"><?php _e( 'Skip to secondary content', 'themename' ); ?></a></div>
 				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'menu','container'       => 'nav','menu_class'=> "menu-container" )); ?>
 			</nav>
 		</header>
 
