5d5af013ad13d5a49ac7eb03d82df4ebdfc6a257
inpixelitrust <stephanie.walter@inpixelitrust.fr>
2012-11-30 14:27:08 +0100

    menu debug
    Now menu works weather user has a custom of default menu

--- header.php
+++ header.php
@@ -70,7 +70,7 @@
 				<div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'themename' ); ?>"><?php _e( 'Skip to primary content', 'themename' ); ?></a></div>
 				<div class="skip-link"><a class="assistive-text" href="#secondary" title="<?php esc_attr_e( 'Skip to secondary content', 'themename' ); ?>"><?php _e( 'Skip to secondary content', 'themename' ); ?></a></div>
 				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'menu', 'container'=> 'div', 'menu_class'=> "menu-container" )); ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'menu', 'container'=> 'div' )); ?>
 			</nav>
 		</header>
 
