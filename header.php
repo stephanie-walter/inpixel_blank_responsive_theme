ae5abf619c02461fb8647cf09a1414467a939a9d
inpixelitrust <stephanie.walter@inpixelitrust.fr>
2013-01-08 14:18:28 +0100

    Update header.php

--- header.php
+++ header.php
@@ -6,8 +6,6 @@
 <!--[if (gt IE 9)|!(IE)]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="chrome=1">
-
 <title><?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
